dfbe979150e488aacde0362481276b2caf9dea42
Luis Felipe <luis.felipeszsv@gmail.com>
2023-10-29 16:02:28 -0300

    feature: remove product

--- src/main/java/com/example/auth/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.auth.repositories;
-
-import com.example.auth.domain.product.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
